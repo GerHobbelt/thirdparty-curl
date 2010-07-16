9a526a6a3c2285f49641e8d05b3b3d817ce52d43
Ben Greear <greearb@candelatech.com>
2010-07-16 11:20:32 -0700

    ssh:  Fix compile error on 64-bit systems.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- lib/ssh.c
+++ lib/ssh.c
@@ -2677,7 +2677,7 @@ static CURLcode ssh_easy_statemach(struct connectdata *conn,
         fd_write = sock;
       /* wait for the socket to become ready */
       Curl_socket_ready(fd_read, fd_write,
-                        left>1000?1000:left); /* ignore result */
+                        (int)(left>1000?1000:left)); /* ignore result */
     }
 #endif
 
