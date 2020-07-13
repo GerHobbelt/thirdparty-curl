6c19651936a3242f0cbb585145aecbbdb37e50c6
Emil Engler <me@emilengler.com>
2020-07-13 14:15:04 +0200

    docs: Add categories to all cmdline opts
    This is the begin of the implementation of "--help me if you can"
    by Daniel Fandrich presented at curl up 2020.
    See: https://bit.ly/2C92PvR

--- docs/cmdline-opts/help.d
+++ docs/cmdline-opts/help.d
@@ -1,6 +1,7 @@
 Long: help
 Short: h
 Help: This help text
+Category: important curl
 ---
 Usage help. This lists all current command line options with a short
 description.
--- docs/cmdline-opts/show-error.d
+++ docs/cmdline-opts/show-error.d
@@ -1,5 +1,6 @@
 Long: show-error
 Short: S
 Help: Show error even when -s is used
+Category: curl
 ---
 When used with --silent, it makes curl show an error message if it fails.
