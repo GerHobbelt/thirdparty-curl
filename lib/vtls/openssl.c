ddb69f791010bce0a05065a484beb59fecf1249b
Erik Olofsson <erik.olofsson@favro.com>
2021-01-15 11:15:31 +0100

    Fix fallback logic

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -3101,14 +3101,14 @@ static CURLcode ossl_connect_step1(struct Curl_easy *data,
   }
 #endif
 
-#ifdef CURL_CA_FALLBACK
+#if defined(CURL_CA_EXTERNAL_FALLBACK) || defined(CURL_CA_FALLBACK)
   if(verifypeer &&
      !ca_info_blob && !ssl_cafile && !ssl_capath && !imported_native_ca) {
-    /* verifying the peer without any CA certificates won't
-       work so use openssl's built in default as fallback */
-#ifdef CURL_CA_EXTERNAL_FALLBACK
+#if defined(CURL_CA_EXTERNAL_FALLBACK)
     curl_ca_external_fallback(SSL_CTX_get_cert_store(backend->ctx));
-#else
+#elif defined(CURL_CA_FALLBACK)
+     /* verifying the peer without any CA certificates won't
+       work so use openssl's built in default as fallback */
     SSL_CTX_set_default_verify_paths(backend->ctx);
 #endif
   }
