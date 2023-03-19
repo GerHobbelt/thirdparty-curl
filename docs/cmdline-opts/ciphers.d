451ba54e478218d9cf726b14f39fdca58898de24
Jay Satiro <raysatiro@yahoo.com>
2023-03-19 04:05:08 -0400

    schannel: fix user-set legacy algorithms in Windows 10 & 11
    - If the user set a legacy algorithm list (CURLOPT_SSL_CIPHER_LIST) then
      use the SCHANNEL_CRED legacy structure to pass the list to Schannel.
    
    - If the user set both a legacy algorithm list and a TLS 1.3 cipher list
      then abort.
    
    Although MS doesn't document it, Schannel will not negotiate TLS 1.3
    when SCHANNEL_CRED is used. That means setting a legacy algorithm list
    limits the user to earlier versions of TLS.
    
    Prior to this change, since 8beff435 (precedes 7.85.0), libcurl would
    ignore legacy algorithms in Windows 10 1809 and later.
    
    Reported-by: zhihaoy@users.noreply.github.com
    
    Fixes #10741
    Closes #xxxx

--- docs/cmdline-opts/ciphers.d
+++ docs/cmdline-opts/ciphers.d
@@ -5,7 +5,7 @@ Arg: <list of ciphers>
 Help: SSL ciphers to use
 Protocols: TLS
 Category: tls
-See-also: tlsv1.3
+See-also: tls13-ciphers tlsv1.3
 Example: --ciphers ECDHE-ECDSA-AES256-CCM8 $URL
 Added: 7.9
 Multi: single
