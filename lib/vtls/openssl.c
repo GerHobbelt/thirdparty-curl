5cbe614b424c941a22150d62840537978c2716d0
Stefan Eissing <stefan@eissing.org>
2023-09-19 08:45:47 +0200

    Simpler version of SSL_shutdown handling as one shot without delays

--- lib/vtls/openssl.c
+++ lib/vtls/openssl.c
@@ -1879,37 +1879,36 @@ static void ossl_close(struct Curl_cfilter *cf, struct Curl_easy *data)
   if(backend->handle) {
     if(cf->next && cf->next->connected) {
       char buf[1024];
-      int nread, i, err, done = FALSE;
-      CURL_TRC_CF(data, cf, "SSL_shutdown() initiate");
-      for(i = 0; !done && i < 100; ++i) {
-        ERR_clear_error();
-        if(SSL_shutdown(backend->handle)) {
-          CURL_TRC_CF(data, cf, "SSL_shutdown() finished successfully");
-          break;
-        }
+      int nread, err;
+      long sslerr;
 
+      ERR_clear_error();
+      if(SSL_shutdown(backend->handle)) {
+        CURL_TRC_CF(data, cf, "SSL shutdown finished");
+      }
+      else {
         nread = SSL_read(backend->handle, buf, (int)sizeof(buf));
         err = SSL_get_error(backend->handle, nread);
         switch(err) {
         case SSL_ERROR_NONE: /* this is not an error */
         case SSL_ERROR_ZERO_RETURN: /* no more data */
-          CURL_TRC_CF(data, cf, "SSL_shutdown() seeing EOF from server");
-          done = TRUE;
+          CURL_TRC_CF(data, cf, "SSL shutdown, EOF from server");
           break;
         case SSL_ERROR_WANT_READ:
           /* SSL has send its notify and now wants to read the reply
            * from the server. We are not really interested in that. */
-          CURL_TRC_CF(data, cf, "SSL_shutdown() has sent everything, "
-                      "not waiting for the answer");
-          done = TRUE;
+          CURL_TRC_CF(data, cf, "SSL shutdown, sent, leaving");
           break;
         case SSL_ERROR_WANT_WRITE:
-          CURL_TRC_CF(data, cf, "SSL_shutdown() still wants to send");
-          Curl_wait_ms(10);
+          CURL_TRC_CF(data, cf, "SSL shutdown, blocked send, leaving");
           break;
-        default: /* error */
-          fprintf(stderr, "SSL_shutdown() returned error, leaving\n");
-          done = TRUE;
+        default:
+          sslerr = ERR_get_error();
+          CURL_TRC_CF(data, cf, "SSL shutdown, error: '%s', errno %d",
+                      (sslerr ?
+                        ossl_strerror(sslerr, buf, sizeof(buf)) :
+                        SSL_ERROR_to_str(err)),
+                      SOCKERRNO);
           break;
         }
       }
